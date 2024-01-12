0091b790acaa37cd72076e8291197f38041b697f
Danylo_Sichkar <126655174+boblmbq@users.noreply.github.com>
2024-01-12 22:44:51 +0100

    authenticational middlewar was added to exercises router

--- routes/api/exersises.js
+++ routes/api/exersises.js
@@ -3,7 +3,11 @@ const { authenticate } = require('../../middlewares');
 const ExersisesController = require('../../controllers/exersises');
 const router = express.Router();
 
-router.get('/categories', ExersisesController.fetchExerciseCategories);
-router.get('/exercises', ExersisesController.fetchExercises);
+router.get(
+  '/categories',
+  authenticate,
+  ExersisesController.fetchExerciseCategories
+);
+router.get('/exercises', authenticate, ExersisesController.fetchExercises);
 
 module.exports = router;
