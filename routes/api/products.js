0c3ae24a26d2d57d83fdfc6eab1f77fcc47d6de9
BogdanK85 <126570087+BogdanK85@users.noreply.github.com>
2024-01-12 18:53:43 +0200

    add products router, model, controller

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,11 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
+const authenticate = require('../../middlewares/authenticate');
+// const {aunthenticate} = require('../../middlewares/authenticate')
+const ctrl = require('../api/product');
 
-// router.get('/');
+router.get('/categories', authenticate, ctrl.getAllCategories);
+
+router.get('/byBloodGroup', authenticate, ctrl.getProductByBloodGroup);
+
+module.exports = router;
