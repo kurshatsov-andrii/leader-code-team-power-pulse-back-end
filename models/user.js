0073a2797f9f1f7609fdf8a95aac021bcee6775c
Svin07 <svin07@ukr.net>
2024-01-12 16:41:27 +0200

    add register and login

--- models/user.js
+++ models/user.js
@@ -1,1 +1,51 @@
-// const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const emailRegexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for user'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      match: emailRegexp,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, 'Set password for user'],
+    },
+
+    token: { type: String, default: '' },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+userSchema.post('save', handleMongooseError);
+
+const User = model('user', userSchema);
+
+module.exports = { User, schemas };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,38 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// router.post('/register');
+const ctrl = require('../../controllers/auth');
+
+const {
+  validateBody,
+  // authenticate,
+  //   upload,
+} = require('../../middlewares');
+
+const { schemas } = require('../../models/user');
+
+router.post(
+  '/register',
+  validateBody(schemas.registerSchema),
+
+  ctrl.register
+);
+
+// router.get('/verify/:verificationToken', ctrl.verifyEmail);
+
+// router.post('/verify', authenticate, ctrl.resendVerifyEmail);
+
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+
+// router.get('/current', authenticate, ctrl.getCurrent);
+
+// router.post('/logout', authenticate, ctrl.logout);
+
+// router.patch(
+//   '/avatar',
+//   authenticate,
+//   upload.single('avatar'),
+//   ctrl.updateAvatar
+// );
+
+module.exports = router;
