78dac87e5e9db6f2f00419ebac2c89506ef659c5
BogdanK85 <126570087+BogdanK85@users.noreply.github.com>
2024-01-13 14:06:15 +0200

    change controller path

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../../middlewares/authenticate');
 // const {aunthenticate} = require('../../middlewares/authenticate')
-const ctrl = require('../api/product');
+const ctrl = require('../../controllers/products');
 
 router.get('/categories', authenticate, ctrl.getAllCategories);
 
