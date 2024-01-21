dfe3c966244eb4505950110e7fd5c305054bd289
Svin07 <svin07@ukr.net>
2024-01-21 12:46:19 +0200

    create constans and update schemas

---
+++ constans/constans.js
@@ -0,0 +1,5 @@
+const emailRegexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const levelsActivity = [1, 2, 3, 4, 5];
+const sex = ['male', 'female'];
+const blood = [1, 2, 3, 4];
+module.exports = { emailRegexp, sex, levelsActivity, blood };
